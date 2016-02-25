60e1f868e135d15d25706b153cbf50fd512c4d4c
alycen <alsunnid@gmail.com>
2016-02-25 17:15:33 +0000

    Included SFML Render Window

--- FYP/FYP.cpp
+++ FYP/FYP.cpp
@@ -1,12 +1,48 @@
-// FYP.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+#pragma comment(lib,"opengl32.lib") 
+#pragma comment(lib,"glu32.lib") 
+
 #include "SFML\Graphics.hpp"
+#include "InputManager.h"
 
 
 int main()
 {
-    return 0;
+	sf::RenderWindow window(sf::VideoMode(1400, 900, 32), "FYP");
+
+	while (window.isOpen())
+	{
+		sf::Event Event;
+		while (window.pollEvent(Event))
+		{
+			InputManager::GetInstance()->UpdatePolledEvents(Event);
+			if (Event.type == sf::Event::Closed)
+				window.close();
+
+			if ((Event.type == sf::Event::KeyPressed) && (Event.key.code == sf::Keyboard::Escape))
+				window.close();
+		}
+
+		InputManager::GetInstance()->UpdateState();
+		window.clear();
+
+		//window.setView(Camera::GetInstance()->getView());
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
 
